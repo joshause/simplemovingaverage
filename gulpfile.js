466f51e259ed323504cc3edddfa17d01ae4d3d3b
Josh Ause <joshua.ause@gmail.com>
2015-06-17 20:38:58 -0700

    npm and travis ci optimization

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,12 @@
 
   var gulp = require('gulp');
   var karma = require('karma').server;
+  var isTravis = process.env.TRAVIS || false;
 
   gulp.task('karma', function(done) {
     karma.start({
-      configFile: __dirname + '/karma.conf.js'
+      configFile: __dirname + '/karma.conf.js',
+      singleRun: isTravis
     }, done);
   });
 
