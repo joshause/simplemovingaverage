f96c0741cf35805e6119b7b842d216c8a2306565
Josh Ause <joshua.ause@gmail.com>
2016-08-06 22:11:32 -0700

    IIFE format update and string doublequote pattern.

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
 (function() {
 
-  'use strict';
+  "use strict";
 
-  var gulp = require('gulp');
-  var karma = require('karma').server;
+  var gulp = require("gulp");
+  var karma = require("karma").server;
   var isTravis = process.env.TRAVIS || false;
 
-  gulp.task('karma', function(done) {
+  gulp.task("karma", function(done) {
     karma.start({
-      configFile: __dirname + '/karma.conf.js',
+      configFile: __dirname + "/karma.conf.js",
       singleRun: isTravis
     }, done);
   });
 
-  gulp.task('default', ['karma']);
+  gulp.task("default", ["karma"]);
 
-})();
+}());
